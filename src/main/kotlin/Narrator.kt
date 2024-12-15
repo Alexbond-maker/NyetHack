a86bcc816fc8053b7c9e5711b692536f1de85900
Alexbond-maker <alexbond.xxi@gmail.com>
2024-12-15 21:16:03 +0300

    Листинг 8.6 Добавление параметра(Nattator.kt)

--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -1,15 +1,16 @@
 package org.example
+
 fun main() {
     narrate("A hero enters the town of Kronstadt. What is their name?")
+}
+
+val narrationModifier: (String)->String = { message ->
+    val numExclamationPoints = 3
+    message.uppercase() + "!".repeat(numExclamationPoints)
 }
 
 fun narrate(
     message: String
 ){
-    val narrationModifier: ()->String = {
-        val numExclamationPoints = 3
-        message.uppercase() + "!".repeat(numExclamationPoints)
-    }
-    println(narrationModifier())
-
+    println(narrationModifier(message))
 }
